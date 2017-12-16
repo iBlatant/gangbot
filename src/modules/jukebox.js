ab9cc0e0f3a0435418defb9d103172b196f21b8a
iBlatant <34513598+iBlatant@users.noreply.github.com>
2017-12-16 10:08:44 +0100

    Update jukebox.js

--- src/modules/jukebox.js
+++ src/modules/jukebox.js
@@ -1,4 +1,3 @@
-
 const __ = require('iterate-js');
 const youtube = require('ytdl-core');
 const spotify = require('../services/spotify.js');
@@ -19,45 +18,45 @@ const infohandlers = {
 
 const handlers = {
 
-    spotify: function(bot, msg, track) {
+    spotify: function(client, msg, track) {
         var uri = track.search.split(':')[1];
-        track.dispatcher = msg.guild.voiceConnection.playStream(spotify(bot, uri), bot.config.stream);
+        track.dispatcher = msg.guild.voiceConnection.playStream(spotify(client, uri), client.config.stream);
     },
 
-    youtube: function(bot, msg, track) {
+    youtube: function(client, msg, track) {
         var url = track.search.contains('://') ? track.search : 'https://www.youtube.com/watch?v=' + track.search;
-        track.dispatcher = msg.guild.voiceConnection.playStream(youtube(url, { audioonly: true }), bot.config.stream);
+        track.dispatcher = msg.guild.voiceConnection.playStream(youtube(url, { audioonly: true }), client.config.stream);
     }
 
 };
 
-module.exports = function(bot) {
-    bot.jukebox = {
+module.exports = function(client) {
+    client.jukebox = {
         play: function(track, msg) {
-            if(bot.config.queue.announce) {
+            if(client.config.queue.announce) {
                 if(track.title)
                     msg.channel.sendMessage(`:musical_note: Now playing: "${track.title}", Requested by: ${track.requestor}`);
             }
             track.playing = true;
             var handler = handlers[track.type];
             if(handler) {
-                handler(bot, msg, track);
+                handler(client, msg, track);
                 if(track.dispatcher) {
                     track.dispatcher.on('end', () => {
                         if(track.playing) {
                             track.playing = false;
-                            var lasttrack = bot.queue.dequeue();
-                            if(bot.config.queue.repeat)
-                                bot.queue.enqueue(lasttrack);
+                            var lasttrack = client.queue.dequeue();
+                            if(client.config.queue.repeat)
+                                client.queue.enqueue(lasttrack);
                             msg.trans = true;
-                            bot.commands.play(msg);
+                            client.commands.play(msg);
                             setTimeout(() => {
                                 track.dispatcher = null;
                             }, 100);
                         }
                     });
                     track.dispatcher.on('error', (err) => {
-                        bot.commands.skip(msg);
+                        client.commands.skip(msg);
                         return msg.channel.sendMessage('error: ' + err);
                     });
                 }
